03ff0e27f5c9e29739e9bc378eb845a6b52971e2
Wichert Akkerman <wichert@wiggy.net>
2020-04-15 21:43:31 +0200

    Allow script-src unsafe-inline for react

--- cmd/health-api-server/csp_middleware.go
+++ cmd/health-api-server/csp_middleware.go
@@ -7,7 +7,7 @@ import (
 func addSecurityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// We allow connect-src to support the webpack-dev-server with hot reload
-		w.Header().Add("Content-Security-Policy", "default-src 'self'; connect-src 'self'; style-src 'self' 'unsafe-inline'; form-action 'none'; frame-ancestors 'none'; block-all-mixed-content;")
+		w.Header().Add("Content-Security-Policy", "default-src 'self'; connect-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; form-action 'none'; frame-ancestors 'none'; block-all-mixed-content;")
 		w.Header().Add("Feature-Policy", "autoplay 'none'; camera 'none'; display-capture 'none'; document-domain 'none'; encrypted-media 'none'; fullscreen 'self'; geolocation 'none'; microphone 'none'; midi 'none'; payment 'none'; vr 'none'")
 		w.Header().Add("X-Frame-Options", "DENY")
 		w.Header().Add("X-Content-Type-Options", "nosniff")
